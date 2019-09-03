168c678e4e939104bd06bb643ed442b0d7f3868d
harikamankala <37373353+harikamankala@users.noreply.github.com>
2019-09-03 08:06:58 +0200

    comments API

--- src/test/java/constants/Constants.java
+++ src/test/java/constants/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
 	public static final String BASE_URI = "http://jsonplaceholder.typicode.com";
 	public static final String GET_USERS = "/users";
 	public static final String GET_POSTS_BY_USERID = "/posts?userId=";
+	public static final String GET_COMMENTS_BY_POSTID = "/comments?postId=";
 	
 }
